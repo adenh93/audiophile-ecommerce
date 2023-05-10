71bcbec7441bf1de1b1921a7c23c981fe9e4b67b
Aden Herold <aden.herold1@gmail.com>
2023-05-10 20:00:36 +1000

    feat: added design system to global styles

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
   title: "Audiophile",
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
