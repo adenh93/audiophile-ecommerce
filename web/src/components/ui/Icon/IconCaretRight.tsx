a589cb9c49c764eccef6924615c076eb9d8bbfce
Aden Herold <aden.herold1@gmail.com>
2023-05-10 21:18:09 +1000

    feat: added IconCaretRight to components/ui/Icon directory

---
+++ web/src/components/ui/Icon/IconCaretRight.tsx
@@ -0,0 +1,20 @@
+import { HTMLAttributes } from "react";
+
+export default function IconCaretRight(props: HTMLAttributes<SVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="7"
+      height="12"
+      viewBox="0 0 7 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.32178 1L6.32178 6L1.32178 11"
+        stroke="currentColor"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
---
+++ web/src/components/ui/Icon/index.ts
@@ -0,0 +1,1 @@
+export { default as IconCaretRight } from "./IconCaretRight";
