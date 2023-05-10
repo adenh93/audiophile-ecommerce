dd5a760b15dc140c78b877327f0f5d04aaa712e9
Aden Herold <aden.herold1@gmail.com>
2023-05-10 22:49:10 +1000

    refactor: added re-exports to components/ui/index.ts module

--- web/src/components/ui/index.ts
+++ web/src/components/ui/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from "./Button";
 export { default as TextButton } from "./TextButton";
+export { default as Input } from "./Input";
+export { default as QuantityPicker } from "./QuantityPicker";
